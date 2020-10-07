56cf658e0eb678a456d2cf26a3eb0c64408d33b8
Simon Buechi <simon.buechi@gmail.com>
2020-10-07 21:10:46 +0200

    Update Shares.sol
    various fixes due to truffle/solc update

--- Shares.sol
+++ Shares.sol
@@ -37,31 +37,32 @@ contract Shares is TokenErc20, owned {
         locked = true;
     }
 
-    function name() public view returns (string memory) {
+    function name() override public view returns (string memory) {
         return "Shareholder Management";
     }
 
-    function symbol() public view returns (string memory) {
+    function symbol() override public view returns (string memory) {
         return "$h";
     }
 
-    function decimals() public view returns (uint8) {
+    function decimals() override public view returns (uint8) {
         return 0;
     }
 
-    function totalSupply() public view returns (uint256) {
+    function totalSupply() override public view returns (uint256) {
         return total;
     }
 
     function balanceOf(address shareholder)
         public
+        override
         view
         returns (uint256 balance)
     {
         return shareholders[shareholder];
     }
 
-    function transfer(address, uint256) public returns (bool success) {
+    function transfer(address, uint256) override public returns (bool success) {
         return false;
     }
 
@@ -69,26 +70,21 @@ contract Shares is TokenErc20, owned {
         address,
         address,
         uint256
-    ) public returns (bool success) {
+    ) override public returns (bool success) {
         return false;
     }
 
-    function approve(address, uint256) public returns (bool success) {
+    function approve(address, uint256) override public returns (bool success) {
         return false;
     }
 
     function allowance(address, address)
         public
+        override
         view
         returns (uint256 remaining)
     {
         return 0;
     }
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(
-        address indexed _owner,
-        address indexed _spender,
-        uint256 _value
-    );
 }
